30d594f6793fde8c0ccf296a30377cddda13ad43
felixbrunner <rubelrennfix@gmail.com>
2022-01-14 11:47:46 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
-import setuptools
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="kungfu-felixbrunner", # Replace with your own username
-    version="0.0.1",
+setup(
+    name="kungfu",
+    version="0.1",
+    description="Toolbox for asset pricing in python/pandas",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="felixbrunner",
     author_email="",
-    description="Toolbox for asset pricing in python/pandas",
-    long_description="",
-    long_description_content_type="text/markdown",
     url="https://github.com/path/",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    install_requires=[
+        "pandas",
+    ],
+    python_requires=">=3.6",
 )
